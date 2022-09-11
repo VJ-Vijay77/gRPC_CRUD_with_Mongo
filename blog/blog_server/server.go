b986ea8a2a4abe8f4332e20946bd889f0cc2af6f
VJ-Vijay77 <vijaydinesh77vj@gmail.com>
2022-09-11 17:14:04 +0530

    server created

--- blog/blog_server/server.go
+++ blog/blog_server/server.go
@@ -4,22 +4,45 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"os"
+	"os/signal"
 
-	"github.com/VJ-Vijay77/gRPC/greet/greetpb"
+	"github.com/VJ-Vijay77/gRPC/blog/blogpb"
 	"google.golang.org/grpc"
 )
 
+type server struct{}
+
 func main() {
+
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
 	fmt.Println("Blog Service Started")
 	lis, err := net.Listen("tcp", "0.0.0.0:50051")
 	if err != nil {
 		log.Fatalf("Failed to listen: %v", err)
 	}
 
-	s := grpc.NewServer()
-	greetpb.RegisterGreetServiceServer(s, &server{})
+	opts := []grpc.ServerOption{}
 
-	if err := s.Serve(lis); err != nil {
-		log.Fatalln(err)
-	}
+	s := grpc.NewServer(opts...)
+	blogpb.RegisterBlogServiceServer(s, &server{})
+
+	go func() {
+		fmt.Println("Starting the server...")
+		if err := s.Serve(lis); err != nil {
+			log.Fatalln(err)
+		}
+	}()
+
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, os.Interrupt)
+
+	<-ch
+	fmt.Print("Stopping the server")
+	s.Stop()
+	fmt.Println("Stopping the listener")
+	lis.Close()
+	fmt.Println("End of Program")
+
 }
